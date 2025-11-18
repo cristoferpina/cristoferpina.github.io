c2ff43b0bbef8ccfbbfc0300b0e3a5797cd504ef
cristoferpina <cristoferpil324@gmail.com>
2025-11-17 21:20:07 -0600

    astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cristoferpina.github.io",
+  base: "cristoferpina.github.io",
   vite: {
     plugins: [tailwindcss()]
   }
